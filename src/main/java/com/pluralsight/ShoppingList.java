f4d82a0cf487d395cfd5ba639489edb6484275f2
taseenmolique <taseenm100@gmail.com>
2024-09-25 15:18:26 -0400

    My Shopping List

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args){
+        System.out.println("Shopping List:");
+        System.out.println("-----------------");
+        System.out.println("Apples");
+        System.out.println("Pears");
+        System.out.println("Tomato");
+        System.out.println("Lemon");
+        System.out.println("Bread");
+        System.out.println("Milk");
+        System.out.println("Ground Beef");
+        System.out.println("Pasta");
+        System.out.println("Onions");
+        System.out.println("Pasta Sauce");
+
+
+    }
+
+}
